24cfd6fe2d57f7ca9827880c933887b2dbdf5132
Andriy Marshalek <amarshalek@ninevastudios.com>
2022-12-01 13:42:49 +0200

    remove ui listener

--- AndroidLibrary/purchasely-lib/src/main/java/com/purchasely/unity/PurchaselyBridge.java
+++ AndroidLibrary/purchasely-lib/src/main/java/com/purchasely/unity/PurchaselyBridge.java
@@ -9,7 +9,6 @@ import androidx.annotation.NonNull;
 import com.purchasely.unity.proxy.EventProxy;
 import com.purchasely.unity.proxy.PlacementContentProxy;
 import com.purchasely.unity.proxy.StartProxy;
-import com.purchasely.unity.proxy.UiListenerProxy;
 import com.purchasely.unity.proxy.UserLoginProxy;
 
 import java.util.ArrayList;
@@ -18,12 +17,9 @@ import java.util.List;
 import io.purchasely.billing.Store;
 import io.purchasely.ext.EventListener;
 import io.purchasely.ext.LogLevel;
-import io.purchasely.ext.PLYAlertMessage;
 import io.purchasely.ext.PLYEvent;
 import io.purchasely.ext.PLYRunningMode;
-import io.purchasely.ext.PLYUIFragmentType;
 import io.purchasely.ext.Purchasely;
-import io.purchasely.ext.UIListener;
 import io.purchasely.google.GoogleStore;
 import io.purchasely.models.PLYError;
 import kotlin.Unit;
@@ -34,17 +30,15 @@ import kotlin.jvm.functions.Function2;
 public class PurchaselyBridge {
 	private StartProxy _startProxy;
 	private final EventProxy _eventProxy;
-	private final UiListenerProxy _uiListenerProxy;
 	private UserLoginProxy _userLoginProxy;
 	static PlacementContentProxy placementContentProxy;
 
 	@Keep
 	public PurchaselyBridge(Activity activity, String apiKey, String userId, boolean readyToPurchase,
 	                        int storeFlags, int logLevel, int runningMode, StartProxy proxy,
-	                        EventProxy eventProxy, UiListenerProxy uiListenerProxy) {
+	                        EventProxy eventProxy) {
 		_startProxy = proxy;
 		_eventProxy = eventProxy;
-		_uiListenerProxy = uiListenerProxy;
 
 		Purchasely.Builder builder = new Purchasely.Builder(activity.getApplicationContext())
 				.apiKey(apiKey)
@@ -52,18 +46,6 @@ public class PurchaselyBridge {
 				.logLevel(parseLogLevel(logLevel))
 				.runningMode(parseMode(runningMode))
 				.stores(parseStoreFlags(storeFlags))
-				.uiListener(new UIListener() {
-					@Override
-					public void onAlert(@NonNull PLYAlertMessage plyAlertMessage) {
-						Alert alert = parseAlert(plyAlertMessage);
-						_uiListenerProxy.onAlert(alert.type, alert.message);
-					}
-
-					@Override
-					public void onFragment(@NonNull androidx.fragment.app.Fragment fragment, @NonNull PLYUIFragmentType plyuiFragmentType) {
-						_uiListenerProxy.onView(parseViewType(plyuiFragmentType));
-					}
-				})
 				.eventListener(new EventListener() {
 					@Override
 					public void onEvent(@NonNull PLYEvent plyEvent) {
@@ -161,61 +143,5 @@ public class PurchaselyBridge {
 	protected void finalize() {
 		placementContentProxy = null;
 		Purchasely.close();
-	}
-
-	private static class Alert {
-		String message;
-		int type;
-	}
-
-	private Alert parseAlert(PLYAlertMessage alertMessage) {
-		Alert alert = new Alert();
-		alert.message = "";
-
-		if (alertMessage instanceof PLYAlertMessage.InAppDeferred) {
-			alert.type = 2;
-		}
-
-		if (alertMessage instanceof PLYAlertMessage.InAppError) {
-			alert.type = 6;
-			PLYError error = ((PLYAlertMessage.InAppError) alertMessage).getError();
-			if (error != null)
-				alert.message = error.toString();
-		}
-
-		if (alertMessage instanceof PLYAlertMessage.InAppOptionChangedSuccess) {
-			alert.type = 10;
-			alert.message = ((PLYAlertMessage.InAppOptionChangedSuccess) alertMessage).getRenewalDate();
-		}
-
-		if (alertMessage instanceof PLYAlertMessage.InAppRestorationError) {
-			alert.type = 5;
-			PLYError error = ((PLYAlertMessage.InAppRestorationError) alertMessage).getError();
-			if (error != null)
-				alert.message = error.toString();
-		}
-
-		if (alertMessage instanceof PLYAlertMessage.InAppRestorationSuccess) {
-			alert.type = 4;
-		}
-
-		if (alertMessage instanceof PLYAlertMessage.InAppSuccess) {
-			alert.type = 1;
-		}
-
-		if (alertMessage instanceof PLYAlertMessage.InAppSuccessUnauthentified) {
-			alert.type = 3;
-		}
-
-		return alert;
-	}
-
-	private int parseViewType(PLYUIFragmentType type) {
-		if (type == PLYUIFragmentType.CANCELLATION_PAGE)
-			return 3;
-		if (type == PLYUIFragmentType.SUBSCRIPTION_LIST)
-			return 0;
-
-		return 1;
 	}
 }
--- AndroidLibrary/purchasely-lib/src/main/java/com/purchasely/unity/proxy/UiListenerProxy.java
+++
@@ -1,6 +0,0 @@
-package com.purchasely.unity.proxy;
-
-public interface UiListenerProxy {
-	void onAlert(int type, String message);
-	void onView(int type);
-}
